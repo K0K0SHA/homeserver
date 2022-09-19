a7f5adc1c999c04efcfa78235f01e1a8b452be5e
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2022-09-19 13:52:33 -0400

    Updating configure.sh
    Added nmap to install list. Nmap can be used by a homeserver setup in order to scan for available clients. Please note that nmap is not necessary in a client-server setup, and probably has little use in this repository.

--- configure.sh
+++ configure.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-# reference https://www.youtube.com/watch?v=qGa94_dUi9I
-apt-get install -y x11vnc
+# homeserver uses x11vnc
+# nmap is used to find other devices on your network (forcefully)
+# reference:     https://www.youtube.com/watch?v=qGa94_dUi9I
+
+apt-get install -y x11vnc nmap
