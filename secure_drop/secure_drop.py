7f0e92a175e7f434799af9ef29877b494d8cedfb
Gul'dan <92905002+K0K0SHA@users.noreply.github.com>
2023-05-16 20:25:39 -0400

    Update secure_drop.py

--- secure_drop/secure_drop.py
+++ secure_drop/secure_drop.py
@@ -1,1 +1,33 @@
+#code header goes here
+import os
+import re
+import platform
+import inquirer
+import socket
+#TODO import netutil
+#TODO implement transfer and test
+#TODO 
 
+
+
+PROTO = "TCP"
+GUI = False
+
+SSH_path = None
+localhost_domain_name = socket.getfqn
+local_device_IP = None
+local_device_list = None
+broadcast_listener_port = 5555
+broadcast_send_port = 666
+
+
+def start_all_servers:
+  receive_server_start
+  send_server_start
+
+def receive_server_start:
+  print("nothing here yet")
+  
+  
+def send_server_start:
+  print("nothing here yet")
